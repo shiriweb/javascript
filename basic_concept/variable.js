4c8487b6a0d949255e021a0a59623f8c006f96bd
unknown <shirisha.singh36@gmail.com>
2024-06-27 20:07:36 +0545

    js

---
+++ basic_concept/variable.js
@@ -0,0 +1,17 @@
+// declaring a variable
+
+var variable = 100;
+console.log(variable);
+var variable = 300;
+console.log(variable);
+
+let digit = 13;
+console.log(digit);
+// let digit = 23; value can be updated but the variable cannot be re-declared
+digit = 23;
+console.log(digit);
+
+const number= 45;
+console.log(number);
+// number= 50; // value cannot be updated nor the variable can be re-declared
+console.log(number);
