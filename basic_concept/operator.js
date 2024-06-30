6a54c8f71e0e09d753a8a8874c95f97ac68f4743
unknown <shirisha.singh36@gmail.com>
2024-06-30 16:50:12 +0545

    operators

---
+++ basic_concept/operator.js
@@ -0,0 +1,54 @@
+// Operators in js
+
+// arithmetica operator 
+// let a= 10;
+// let b = 12;
+// let c = a + b;
+// console.log("a = ", a,  "& b = ", b);
+// console.log("a + b =", c);
+
+// let a= 10;
+// let b = 12;
+// let c = a - b;
+// console.log("a = ", a,  "& b = ", b);
+// console.log("a - b =", c);
+
+// let a= 10;
+// let b = 12;
+// let c = a * b;
+// console.log("a = ", a,  "& b = ", b);
+// console.log("a * b =", c);
+
+// let a= 10;
+// let b = 12;
+// let c = a / b;
+// console.log("a = ", a,  "& b = ", b);
+// console.log("a / b =", c);
+
+// let a= 10;
+// let b = 12;
+// let c = a % b;
+// console.log("a = ", a,  "& b = ", b);
+// console.log("a % b =", c);
+
+// let a= 10;
+// let b = 12;
+// let c = a ** b;
+// console.log("a = ", a,  "& b = ", b);
+// console.log("a ** b =", c);
+
+// let a= 10;
+// let b = 12;
+// b++;
+// console.log(b);
+// ++b
+// console.log(b);
+
+// let a= 10;
+// let b = 12;
+// b--;
+// console.log(a--);
+// console.log(a++);
+// console.log(b);
+// --b;
+// console.log(b);
